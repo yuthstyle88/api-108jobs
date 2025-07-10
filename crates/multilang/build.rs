2ab7aa450c18152d609c42b39ee2d94779356956
koey <thekhoi.k@gmail.com>
2025-07-10 10:31:02 +0700

    remove lemmy translations

--- crates/multilang/build.rs
+++ crates/multilang/build.rs
@@ -3,15 +3,11 @@ use std::fs::read_dir;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let mut config = rosetta_build::config();
 
-  for path in read_dir("translations/email/")? {
+  for path in read_dir("translations/multilingual/")? {
     let path = path?.path();
     if let Some(name) = path.file_name() {
-      let mut lang = name.to_string_lossy().to_string().replace(".json", "");
+      let lang = name.to_string_lossy().to_string().replace(".json", "");
 
-      // Rename Chinese simplified variant because there is no translation zh
-      if lang == "zh_Hans" {
-        lang = "zh".to_string();
-      }
       // Rosetta doesnt support these language variants.
       if lang.contains('_') {
         continue;
---
+++ crates/multilang/translations/README.md
@@ -0,0 +1,7 @@
+# lemmy-translations
+
+A store for lemmy's translations in i18n json format.
+
+---
+
+**Translations** are made by **Weblate** on this site: https://weblate.join-lemmy.org/projects/lemmy/

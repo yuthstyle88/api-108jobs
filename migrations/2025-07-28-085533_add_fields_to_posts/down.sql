a92df025717f5c2860e574b9d501a4f5eb278441
koey <thekhoi.k@gmail.com>
2025-07-29 11:40:40 +0700

    remove ap_id post in migration

--- migrations/2025-07-28-085533_add_fields_to_posts/down.sql
+++ migrations/2025-07-28-085533_add_fields_to_posts/down.sql
@@ -1,8 +1,11 @@
 ALTER TABLE post
-DROP COLUMN slug,
-  DROP COLUMN is_english_required,
-  DROP COLUMN budget,
-  DROP COLUMN deadline,
-  DROP COLUMN intended_use;
+DROP COLUMN IF EXISTS is_english_required,
+  DROP COLUMN IF EXISTS budget,
+  DROP COLUMN IF EXISTS deadline,
+  DROP COLUMN IF EXISTS job_type,
+  DROP COLUMN IF EXISTS intended_use,
+  ADD COLUMN ap_id TEXT,
+  ADD CONSTRAINT idx_post_ap_id UNIQUE (ap_id);
 
-DROP TYPE intended_use_enum;
+DROP TYPE IF EXISTS job_type_enum;
+DROP TYPE IF EXISTS intended_use_enum;

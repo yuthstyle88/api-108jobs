b02f76c4545b9aed6faa36096725f6396346ae99
koey <thekhoi.k@gmail.com>
2025-08-04 17:42:24 +0700

    add pending to post migration

--- migrations/2025-07-28-085533_add_fields_to_post/up.sql
+++ migrations/2025-07-28-085533_add_fields_to_post/up.sql
@@ -2,6 +2,7 @@ CREATE TYPE intended_use_enum AS ENUM ('Business', 'Personal', 'Unknown');
 CREATE TYPE job_type_enum AS ENUM ('Freelance', 'Contract', 'PartTime', 'FullTime');
 
 ALTER TABLE post
+  ADD COLUMN pending boolean NOT NULL DEFAULT false,
   ADD COLUMN is_english_required boolean NOT NULL DEFAULT false,
   ADD COLUMN budget double precision NOT NULL DEFAULT 0,
   ADD COLUMN deadline timestamptz,

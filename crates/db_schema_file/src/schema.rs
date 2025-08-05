1085e4728c1ec2131c0e9ac8a7a7c87f5aa0ee2d
Khoi Tran <khoitran.lord@gmail.com>
2025-08-05 20:26:52 +0700

    Remove files

--- crates/db_schema_file/src/schema.rs
+++ crates/db_schema_file/src/schema.rs
@@ -395,7 +395,7 @@ diesel::table! {
         id -> Int4,
         local_user_id -> Int4,
         email -> Text,
-        verification_code -> Text,
+        verification_token -> Text,
         published_at -> Timestamptz,
     }
 }
@@ -507,7 +507,6 @@ diesel::table! {
         users_active_month -> Int8,
         users_active_half_year -> Int8,
         disable_email_notifications -> Bool,
-        verify_with_otp -> Bool,
     }
 }
 

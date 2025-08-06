b94a72cd46ad755e1ecfd27987523241e656ce7b
Khoi Tran <khoitran.lord@gmail.com>
2025-08-06 15:24:31 +0700

    update

--- migrations/2025-08-05-004058_add_verification_fields_to_user_bank_accounts/down.sql
+++ migrations/2025-08-05-004058_add_verification_fields_to_user_bank_accounts/down.sql
@@ -1,4 +1,1 @@
--- Remove verification fields from user_bank_accounts table
-DROP INDEX IF EXISTS idx_user_bank_accounts_is_verified;
 ALTER TABLE user_bank_accounts DROP COLUMN IF EXISTS verification_image_path;
-ALTER TABLE user_bank_accounts DROP COLUMN IF EXISTS is_verified;

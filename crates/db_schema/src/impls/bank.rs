60781925a36efbbcf7f62dd864bce7e5194180e9
yongyutjantaboot <yuth_style88@hotmail.com>
2025-08-12 10:15:33 +0700

    update list bank

--- crates/db_schema/src/impls/bank.rs
+++ crates/db_schema/src/impls/bank.rs
@@ -13,12 +13,9 @@ use diesel::QueryDsl;
 use diesel_async::RunQueryDsl;
 #[cfg(feature = "full")]
 use lemmy_db_schema_file::schema::banks;
-use lemmy_db_schema_file::schema::{certificates, user_bank_accounts};
 #[cfg(feature = "full")]
 use lemmy_utils::error::{FastJobErrorExt, FastJobErrorType, FastJobResult};
-use crate::newtypes::PersonId;
 use crate::source::bank::BanksResponse;
-use crate::source::certificates::{CertificateView, Certificates};
 
 #[cfg(feature = "full")]
 impl Crud for Bank {
@@ -49,32 +46,28 @@ impl Crud for Bank {
   }
 }
 impl Bank {
-    pub async fn query_with_order_by(
-      pool: &mut DbPool<'_>,
-      order_by: Option<String>,
-    ) -> FastJobResult<BanksResponse> {
-      let conn = &mut get_conn(pool).await?;
+  pub async fn query_with_order_by(
+    pool: &mut DbPool<'_>,
+    order_by: Option<String>,
+  ) -> FastJobResult<BanksResponse> {
+    let conn = &mut get_conn(pool).await?;
 
-      let mut query = banks::table
-      .into_boxed();
+    let mut query = banks::table.into_boxed();
 
-      let items: Vec<Bank> = query
-      .load(conn)
-      .await?;
-      match order_by.as_deref() {
-        Some("bank_name_asc") => {
-          query = query.order(banks::name.asc());
-        }
-        Some("bank_name_desc") => {
-          query = query.order(banks::name.desc());
-        }
-        _ => {
-          // Default: is_default desc, created_at desc
-          query = query
-          .order(banks::created_at.desc());
-        }
+    match order_by.as_deref() {
+      Some("bank_name_asc") => {
+        query = query.order(banks::name.asc());
       }
-      Ok(BanksResponse{ banks: items })
+      Some("bank_name_desc") => {
+        query = query.order(banks::name.desc());
+      }
+      _ => {
+        // Default: newest first
+        query = query.order(banks::created_at.desc());
+      }
     }
 
+    let items: Vec<Bank> = query.load(conn).await?;
+    Ok(BanksResponse { banks: items })
+  }
 }

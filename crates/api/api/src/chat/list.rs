1a67d0d15c969fd07b736570483b43625a8f65fc
koey <thekhoi.k@gmail.com>
2025-09-03 00:05:48 +0700

    guard for list room

--- crates/api/api/src/chat/list.rs
+++ crates/api/api/src/chat/list.rs
@@ -1,17 +1,13 @@
 use actix_web::web::{Data, Json, Query};
 use lemmy_api_utils::context::FastJobContext;
 use lemmy_db_schema::source::chat_participant::ChatParticipant;
-use lemmy_db_schema::{
-  newtypes::LocalUserId,
-  source::chat_room::ChatRoom
-  ,
-};
+use lemmy_db_schema::source::chat_room::ChatRoom;
+use lemmy_db_views_local_user::LocalUserView;
 use lemmy_utils::error::FastJobResult;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 pub struct ListUserChatRooms {
-  pub user_id: LocalUserId,
   pub limit: Option<i64>,
 }
 
@@ -23,11 +19,12 @@ pub struct ListUserChatRoomsResponse {
 pub async fn list_chat_rooms(
   data: Query<ListUserChatRooms>,
   context: Data<FastJobContext>,
+  local_user_view: LocalUserView
 ) -> FastJobResult<Json<ListUserChatRoomsResponse>> {
   let limit = data.limit.unwrap_or(50);
   let rooms = ChatParticipant::list_rooms_for_user(
     &mut context.pool(),
-    data.user_id,
+    local_user_view.local_user.id,
     limit,
   )
   .await?;

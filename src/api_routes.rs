d732085f9b3f8d4bc83ba334a40d51ec3e8491f0
koey <thekhoi.k@gmail.com>
2025-09-22 10:47:48 +0700

    add route for cancel job

--- src/api_routes.rs
+++ src/api_routes.rs
@@ -12,8 +12,8 @@ use lemmy_api::local_user::review::{list_user_reviews, submit_user_review};
 use lemmy_api::local_user::update_term::update_term;
 use lemmy_api::local_user::wallet::get_wallet;
 use lemmy_api::local_user::workflow::{
-  approve_quotation, approve_work, create_quotation, get_billing_by_comment, request_revision,
-  start_workflow, submit_start_work, submit_work, update_budget_plan_status,
+  approve_quotation, approve_work, cancel_job, create_quotation, get_billing_by_comment,
+  request_revision, start_workflow, submit_start_work, submit_work, update_budget_plan_status,
 };
 use lemmy_api::{
   comment::{
@@ -316,7 +316,7 @@ pub fn config(cfg: &mut ServiceConfig, rate_limit: &RateLimit) {
             .service(scope("/wallet").route("", get().to(get_wallet)))
             // Bank account management scope
             .service(scope("/banks").route("", get().to(list_banks)))
-            // Services scope for freelancer service delivery
+            // Services scope for workflow service
             .service(
               scope("/services")
                 .route("/create-invoice", post().to(create_quotation))
@@ -327,7 +327,8 @@ pub fn config(cfg: &mut ServiceConfig, rate_limit: &RateLimit) {
                 .route("/request-revision", post().to(request_revision))
                 .route("/approve-work", post().to(approve_work))
                 .route("/budget-plan", put().to(update_budget_plan_status))
-                .route("/billing/by-comment", get().to(get_billing_by_comment)),
+                .route("/billing/by-comment", get().to(get_billing_by_comment))
+                .route("cancel-job", post().to(cancel_job)),
             )
             // Account settings import / export have a strict rate limit
             .service(scope("/settings").wrap(rate_limit.import_user_settings()))
